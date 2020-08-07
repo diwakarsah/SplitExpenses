3dceb0d6c4dfd36ea169a9d6cb02761afecb9396
Diwakar Sah <diw_shah@hotmail.com>
2020-08-07 17:20:40 +1000

    Updates

---
+++ precache-manifest.59aca40a5ec02631992731906d0baca3.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f5fdcfcb3b44aab6d88d27a25415e23",
+    "url": "/SplitExpenses/index.html"
+  },
+  {
+    "revision": "c4f8744f6b5aa7f43095",
+    "url": "/SplitExpenses/static/css/5.cbeaa07e.chunk.css"
+  },
+  {
+    "revision": "d9230a71758125731ed3",
+    "url": "/SplitExpenses/static/css/6.8dfec390.chunk.css"
+  },
+  {
+    "revision": "1dfb5c662dee41c2f34b",
+    "url": "/SplitExpenses/static/css/main.97d160ea.chunk.css"
+  },
+  {
+    "revision": "077b74d65779f7b8b6be",
+    "url": "/SplitExpenses/static/js/0.8484e262.chunk.js"
+  },
+  {
+    "revision": "87868aa8ef36e9dc3f86c9c4e20fc052",
+    "url": "/SplitExpenses/static/js/0.8484e262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51313386029f08b6b171",
+    "url": "/SplitExpenses/static/js/1.ba19011f.chunk.js"
+  },
+  {
+    "revision": "21a581182b32546e7410",
+    "url": "/SplitExpenses/static/js/10.0c2ed9d2.chunk.js"
+  },
+  {
+    "revision": "5b2b7693c2c47bd83869",
+    "url": "/SplitExpenses/static/js/11.60e3e6e6.chunk.js"
+  },
+  {
+    "revision": "1bf72966e360ae500d3a",
+    "url": "/SplitExpenses/static/js/12.0a3a31f9.chunk.js"
+  },
+  {
+    "revision": "4494aaa54944d68d1306",
+    "url": "/SplitExpenses/static/js/13.58ef6d95.chunk.js"
+  },
+  {
+    "revision": "b619aa9f634e8399c923",
+    "url": "/SplitExpenses/static/js/14.9ad4b6c7.chunk.js"
+  },
+  {
+    "revision": "d3696106a8609dc60f75",
+    "url": "/SplitExpenses/static/js/15.40d09dda.chunk.js"
+  },
+  {
+    "revision": "e4ab9a1bcf98430059e5",
+    "url": "/SplitExpenses/static/js/16.90208716.chunk.js"
+  },
+  {
+    "revision": "9b1e47dc0f4715bed094",
+    "url": "/SplitExpenses/static/js/17.61ad2574.chunk.js"
+  },
+  {
+    "revision": "01337867faec69f7580c",
+    "url": "/SplitExpenses/static/js/18.f83315be.chunk.js"
+  },
+  {
+    "revision": "a0d18026be3bc57ae1a0",
+    "url": "/SplitExpenses/static/js/19.2f2e1bec.chunk.js"
+  },
+  {
+    "revision": "ed5e82e531580b5c1919",
+    "url": "/SplitExpenses/static/js/2.babbd289.chunk.js"
+  },
+  {
+    "revision": "db5ea00c97a4d929ddd6",
+    "url": "/SplitExpenses/static/js/20.39b84f8c.chunk.js"
+  },
+  {
+    "revision": "c4f8744f6b5aa7f43095",
+    "url": "/SplitExpenses/static/js/5.88fd9ffa.chunk.js"
+  },
+  {
+    "revision": "d9230a71758125731ed3",
+    "url": "/SplitExpenses/static/js/6.3265e073.chunk.js"
+  },
+  {
+    "revision": "14060d5403b5cb599a71704f7b391b06",
+    "url": "/SplitExpenses/static/js/6.3265e073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d66e563476bbb0b0503",
+    "url": "/SplitExpenses/static/js/7.23204d9e.chunk.js"
+  },
+  {
+    "revision": "f20cd17effdd8edd17d7",
+    "url": "/SplitExpenses/static/js/8.3c33677a.chunk.js"
+  },
+  {
+    "revision": "5fb058d4952dedd16c46",
+    "url": "/SplitExpenses/static/js/9.9eacf531.chunk.js"
+  },
+  {
+    "revision": "1dfb5c662dee41c2f34b",
+    "url": "/SplitExpenses/static/js/main.b8936b9b.chunk.js"
+  },
+  {
+    "revision": "6cf9e79f7f905c8bf4b6",
+    "url": "/SplitExpenses/static/js/runtime-main.724c804f.js"
+  },
+  {
+    "revision": "44fb1930f2ccef0d2185e269c39a6602",
+    "url": "/SplitExpenses/static/media/bg-img(1).44fb1930.jpg"
+  },
+  {
+    "revision": "ab95f5ce12e1859ac0f5ae374c2ecebe",
+    "url": "/SplitExpenses/static/media/happy.ab95f5ce.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ffb602c709c9c4f00403555afeed63d.js"
+  "/SplitExpenses/precache-manifest.59aca40a5ec02631992731906d0baca3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SplitExpenses/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
